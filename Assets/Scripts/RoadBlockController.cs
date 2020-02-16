01cb0d9aaff194064e0916ee1927bec6df5a01f7
gahgsp <kelvinbianchi@gmail.com>
2020-02-16 16:51:47 -0300

    Finished infinite generation based on the previous block

---
+++ Assets/Scripts/RoadBlockController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class RoadBlockController : MonoBehaviour
+{
+
+    public enum Direction
+    {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    }
+
+    [SerializeField] Direction entryDirection;
+    [SerializeField] Direction exitDirection;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Direction GetEntryDirection()
+    {
+        return entryDirection;
+    }
+
+    public Direction GetExitDirection()
+    {
+        return exitDirection;
+    }
+}
--- Assets/Scripts/RoadManager.cs
+++ Assets/Scripts/RoadManager.cs
@@ -1,17 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 public class RoadManager : MonoBehaviour
 {
-
-    [SerializeField] GameObject roadBlock;
+    [SerializeField] GameObject[] roadBlocks;
 
     private GameObject _lastSpawnedBlock;
-    
+
     private void Awake()
     {
         for (int i = 0; i < 5; i++)
         {
-           SpawnRoadBlock();
+            SpawnRoadBlock();
         }
     }
 
@@ -30,31 +30,64 @@ public class RoadManager : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
     }
-    
+
     // FIXME: Redo this function
     private void SpawnRoadBlock()
     {
         if (_lastSpawnedBlock != null)
         {
-            var spawnedBlock = Instantiate(roadBlock,
-                _lastSpawnedBlock.transform.position +
-                new Vector3(0, 0, _lastSpawnedBlock.GetComponent<Collider>().bounds.extents.z * 2), Quaternion.identity, transform);
+            var nextBlockRoad = GetBestRoadBlock();
+            var spawnedBlock = Instantiate(nextBlockRoad,
+                nextBlockRoad.transform.position, Quaternion.identity, transform);
             _lastSpawnedBlock = spawnedBlock;
         }
         else
         {
+            var roadBlock = roadBlocks[2];
             var spawnedBlock = Instantiate(roadBlock,
                 new Vector3(transform.position.x, transform.position.y,
                     roadBlock.GetComponent<Collider>().bounds.extents.z * 2), Quaternion.identity, transform);
             _lastSpawnedBlock = spawnedBlock;
         }
+    }
+
+    private GameObject GetBestRoadBlock()
+    {
+        var lastRoadBlockExitDirection = _lastSpawnedBlock.GetComponent<RoadBlockController>().GetExitDirection();
+        var bestEntryDirection = RoadBlockController.Direction.NORTH;
+        var lastRoadBlockPosition = _lastSpawnedBlock.transform.position;
+        switch (lastRoadBlockExitDirection)
+        {
+            case RoadBlockController.Direction.EAST:
+                bestEntryDirection = RoadBlockController.Direction.WEST;
+                lastRoadBlockPosition +=
+                    new Vector3(_lastSpawnedBlock.GetComponent<Collider>().bounds.extents.x * 2, 0, 0);
+                break;
+            case RoadBlockController.Direction.WEST:
+                bestEntryDirection = RoadBlockController.Direction.EAST;
+                lastRoadBlockPosition +=
+                    new Vector3(_lastSpawnedBlock.GetComponent<Collider>().bounds.extents.x * 2 * -1, 0, 0);
+                break;
+            case RoadBlockController.Direction.NORTH:
+                bestEntryDirection = RoadBlockController.Direction.SOUTH;
+                lastRoadBlockPosition +=
+                    new Vector3(0, 0, _lastSpawnedBlock.GetComponent<Collider>().bounds.extents.z * 2);
+                break;
+        }
+
+        var possibleRoadBlocks = roadBlocks
+            .Where(roadBlock => roadBlock.GetComponent<RoadBlockController>().GetEntryDirection() == bestEntryDirection)
+            .ToList();
+
+        var bestRoadBlockIndex = Random.Range(0, possibleRoadBlocks.Count);
+        var bestRoadBlock = possibleRoadBlocks[bestRoadBlockIndex];
+        bestRoadBlock.transform.position = lastRoadBlockPosition;
+        return bestRoadBlock;
     }
 }
