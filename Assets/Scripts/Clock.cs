3ea274c584a58df011dc8b7d4362c0e27b58fc03
gahgsp <kelvinbianchi@gmail.com>
2020-02-18 21:10:57 -0300

    Added a current hour display on the UI

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,63 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+
+    // Cached references.
+    private DayTimeCycle _dayTimeCycleController;
+    private TextMeshProUGUI _clockText;
+
+    private bool _needToClearHourDisplay;
+    
+    // Awake is called when the script instance is being loaded
+    void Awake()
+    {
+        _dayTimeCycleController = FindObjectOfType<DayTimeCycle>();
+        _clockText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateClock();
+    }
+
+    private void UpdateClock()
+    {
+        DisplayHour();
+        if (_needToClearHourDisplay)
+        {
+            Invoke(nameof(ClearHour), 2f);
+        }
+    }
+    
+    private void DisplayHour()
+    {
+        var currentHourOfDay = Mathf.Abs(24 * _dayTimeCycleController.GetCurrentHourOfDay());
+        // TODO: Create a SAFETY CONST to explain this 0.2f that is added!
+        if (currentHourOfDay >= 6f && currentHourOfDay < 6.2f)
+        {
+            _clockText.text = "06:00";
+            _needToClearHourDisplay = true;
+        } else if (currentHourOfDay >= 12f && currentHourOfDay < 12.2f)
+        {
+            _clockText.text = "12:00";
+            _needToClearHourDisplay = true;
+        } else if (currentHourOfDay >= 18f && currentHourOfDay < 18.2f)
+        {
+            _clockText.text = "18:00";
+            _needToClearHourDisplay = true;
+        } else if (currentHourOfDay >= 0f && currentHourOfDay < 0.2f)
+        {
+            _clockText.text = "00:00";
+            _needToClearHourDisplay = true;
+        }
+    }
+
+    private void ClearHour()
+    {
+        _clockText.text = "";
+        _needToClearHourDisplay = false;
+    }
+}
--- Assets/Scripts/DayTimeCycle.cs
+++ Assets/Scripts/DayTimeCycle.cs
@@ -5,19 +5,32 @@ public class DayTimeCycle : MonoBehaviour
 
     [SerializeField] int completeDayInSeconds = 30;
     
-    private float _currHourOfDay = 0.25f; // The sun always starts at 06:00 AM
+    private float _currentHourOfDay = 0.25f; // The sun always starts at 06:00 AM
+    private float _clockWiseCurrentHourOfDay = 0.25f; // The sun always starts at 06:00 AM
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         // We add 90º to make the sun rise at 180º (0.25 of a day that is equal to 06:00 AM)
-        transform.localRotation = Quaternion.Euler((_currHourOfDay * 360f) + 90, 90, 0);
-        _currHourOfDay -= (Time.deltaTime / completeDayInSeconds);
+        transform.localRotation = Quaternion.Euler((_currentHourOfDay * 360f) + 90, 90, 0);
+        
+        // This variable is used to handle the inverted Unit Circle on Unity, so we can rotate the Sun as it does
+        // on real life, from East to West
+        _currentHourOfDay -= (Time.deltaTime / completeDayInSeconds);
+        
+        // And this is where we handle the hours "naturally" so we can use on our clock
+        _clockWiseCurrentHourOfDay += (Time.deltaTime / completeDayInSeconds);
+
+        // Resets the day after it reaches 00:00 PM
+        if (_clockWiseCurrentHourOfDay >= 1)
+        {
+            _clockWiseCurrentHourOfDay = 0;
+        }
+        
+    }
+
+    public float GetCurrentHourOfDay()
+    {
+        return _clockWiseCurrentHourOfDay;
     }
 }
