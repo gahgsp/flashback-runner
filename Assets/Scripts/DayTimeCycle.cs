1fdddabca8d0c6ac2aac75b697d83d973c1e84a0
gahgsp <kelvinbianchi@gmail.com>
2020-02-22 11:33:15 -0300

    From 6 AM to 6 PM the light intensity of the sun is higher

--- Assets/Scripts/DayTimeCycle.cs
+++ Assets/Scripts/DayTimeCycle.cs
@@ -4,9 +4,17 @@ public class DayTimeCycle : MonoBehaviour
 {
 
     [SerializeField] int completeDayInSeconds = 30;
+
+    private Light _sunLight;
     
     private float _currentHourOfDay = 0.25f; // The sun always starts at 06:00 AM
     private float _clockWiseCurrentHourOfDay = 0.25f; // The sun always starts at 06:00 AM
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sunLight = GetComponent<Light>();
+    }
     
     // Update is called once per frame
     void Update()
@@ -20,13 +28,33 @@ public class DayTimeCycle : MonoBehaviour
         
         // And this is where we handle the hours "naturally" so we can use on our clock
         _clockWiseCurrentHourOfDay += (Time.deltaTime / completeDayInSeconds);
-
+        
+        // Adjust the Sun light intensity by hour
+        UpdateSunIntensity();
+        
         // Resets the day after it reaches 00:00 PM
+        ResetDayHour();
+        
+    }
+
+    private void UpdateSunIntensity()
+    {
+        if (_clockWiseCurrentHourOfDay >= 0.25f && _clockWiseCurrentHourOfDay <= 0.75f)
+        {
+            _sunLight.intensity = 5f;
+        }
+        else
+        {
+            _sunLight.intensity = 1f;
+        }
+    }
+
+    private void ResetDayHour()
+    {
         if (_clockWiseCurrentHourOfDay >= 1)
         {
             _clockWiseCurrentHourOfDay = 0;
         }
-        
     }
 
     public float GetCurrentHourOfDay()
